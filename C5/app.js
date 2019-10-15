b40b7e473a6487c200bc31622209ba9e72e55b35
Braynner Teixeira <42365689+BraynnerTeixeira@users.noreply.github.com>
2019-10-15 10:12:49 -0300

    inicial

---
+++ C5/app.js
@@ -0,0 +1,30 @@
+function Over(obj) {
+	
+	obj.style.backgroundColor="#0162b3";
+	obj.style.color="#fff";
+
+}
+
+function Out(obj) {
+	
+	obj.style.backgroundColor="#f2faff";
+	obj.style.color="#0162b3";
+
+}
+
+function AbrirF(){
+
+window.open('https://www.facebook.com/braynnerteixeira');
+}
+
+function AbrirI(){
+
+	window.open('https://www.instagram.com/braynnerteixeira');
+	
+}
+
+function Descer(){
+
+	window.location.href='#contato';
+	
+}
